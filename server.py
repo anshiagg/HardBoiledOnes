97f5494491118813241cb877314cbc7077cad635
Jack Gifford <shmacktus@gmail.com>
2018-08-04 20:39:16 +1000

    Added table

--- server.py
+++ server.py
@@ -23,3 +23,37 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+def test():
+    course_dictionary = {}
+
+    def build_class(is_lecture, start_time, hours, day):
+        if is_lecture:
+            return Lecture(start_time, hours, day)
+
+        else:
+            return Tutorial(start_time, hours, day)
+    
+    with open("MockData.txt", 'r') as file:
+        spamreader = csv.reader(file, delimiter='|')
+        title=True
+        for row in spamreader:
+            if title:
+                title = False
+                continue
+
+            course_code = row[6]
+            activity = row[1]
+
+            new_class = build_class(activity == "Lecture", row[4], row[5], row[3])
+
+            if course_code not in course_dictionary:
+                course_dictionary[course_code] = Course(course_code, None, [])
+
+        
+            if activity == "Lecture":
+                course_dictionary[course_code].Lecture = new_class
+
+            else:
+                course_dictionary[course_code].tutorials.append(new_class)
