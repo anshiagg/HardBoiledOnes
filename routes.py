108665e8119127cbe6044bb7af6a27029c3c8ad0
IVESSSSSSSSSSS <37000892+IVESSSSSSSSSSS@users.noreply.github.com>
2018-08-04 17:01:18 +1000

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,17 +1,61 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, RadioField
+from wtforms.validators import InputRequired, Email, Length
+from werkzeug.security import generate_password_hash, check_password_hash
+from server import *
+from flask_login import login_user, login_required, logout_user, current_user
 from datetime import datetime, timedelta
-from flask_login import login_user, logout_user, current_user, login_required
-from server import app
 
-@app.route('/404')
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+@app.route("/404")
 @app.errorhandler(404)
 def page_not_found(e=None):
-    return render_template('404.html'), 404
+    return render_template("base.html", content="404")
 
 @app.route('/')
 def index():
-	return render_template('home.html')
+	return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+            else:
+                return render_template("login.html", form=form, login_failed=True)
+        else:
+            return render_template('login.html', form=form, registered=False)
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for("index"))
+    return render_template('signup.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
--- server.py
+++ server.py
@@ -1,6 +1,25 @@
-from flask import Flask 
-from flask_login import LoginManager
-
+from flask_bootstrap import Bootstrap
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
 
 app = Flask(__name__)
-app.secret_key = 'this_is_a_secret_key'
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'secret!'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/d/OneDrive/GITHUB/Python/py_flask/finish/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\OneDrive\\GITHUB\\Python\\hackathon\\\HardBoiledOnes\\database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
